40b4c2a448bd8a643c4355b64d3942478f1b1236
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-11-03 22:10:21 -0500

    complete algos/debugging/strings-xor

---
+++ algos/debugging/strings-xor.py
@@ -0,0 +1,13 @@
+# Strings XOR
+
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res += '0';
+        else:
+            res += '1';
+
+    return res
+
+# Passed 10 test cases successfully.
