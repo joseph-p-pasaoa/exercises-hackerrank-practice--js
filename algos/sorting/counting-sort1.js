5fbc847204be173b54300fb3757e9da7ca462de1
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-11-05 21:45:18 -0500

    complete algos/sorting/counting-sort1

---
+++ algos/sorting/counting-sort1.js
@@ -0,0 +1,10 @@
+// Counting Sort
+
+function countingSort(arr) {
+  const counts = new Array(100).fill(0);
+  arr.forEach(num => counts[num] += 1);
+
+  return counts;
+}
+
+// Passed 6 test cases successfully.
